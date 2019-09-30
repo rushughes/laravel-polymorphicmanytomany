dcac1c9a1ad72719fc2ecbe66183fd55d0f97b79
Rus Hughes <russell.hughes@gmail.com>
2019-09-30 16:53:58 +0200

    s17 l118

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,24 @@ Route::get('/create', function () {
   $tag2 = Tag::find(2);
   $video->tags()->save($tag2);
 });
+
+Route::get('/read/{id}', function ($id) {
+  $post = Post::findOrFail($id);
+  foreach($post->tags as $tag) {
+    $tag->whereName('tag-one')->update(['name'=>'Updated']);
+    echo $tag;
+  }
+});
+
+
+Route::get('/update/{id}/{tag_id}', function ($id, $tag_id) {
+  $post = Post::findOrFail($id);
+  // foreach($post->tags as $tag) {
+  //   $tag->whereName('tag-one')->update(['name'=>'Updated']);
+  //   echo $tag;
+  // }
+  $tag = Tag::find($tag_id);
+  // $post->tags()->save($tag);
+  // $post->tags()->attach($tag);
+  $post->tags()->sync($tag);
+});
